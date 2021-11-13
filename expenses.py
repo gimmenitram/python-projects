42d91cf196c7832baae4ce34522955caf629791e
gimmenitram <martin.bangayan@gmail.com>
2021-11-13 21:05:16 +1300

    added comment for approaches/sections

--- expenses.py
+++ expenses.py
@@ -1,10 +1,13 @@
 expenses = [10.50, 8, 5, 15, 20, 5, 3]
 
+# Loop approach
+
 # sum = 0
 
 # for x in expenses:
 #     sum = sum + x
 # print("You spent $", sum, " on lunch this week.")
 
+# Library/Function approach
 total = sum(expenses)
 print("You spent $", total, " on lunch this week.", sep='')
