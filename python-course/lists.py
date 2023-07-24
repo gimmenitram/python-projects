30e6a3490e049adb0a6a6d82f46d6eb9e4a46ebb
Martin <martin.bangayan@gmail.com>
2023-07-24 21:32:53 +1200

    commit string_index.py, lists.py

---
+++ python-course/lists.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3, "a", "b", "c"]
+list2 = [11, 22, 33, "aa", "bb", "cc"]
+
+print(list1)
+print(list2)
+
+print(list1*2) # duplicate
+print(list1+list2) # concatenate
+list2[3] = "xx" # replace value
+print(list2)
---
+++ python-course/string_index.py
@@ -0,0 +1,9 @@
+str1 = "this"
+str2 = "really"
+str3 = "fly"
+
+print (str1, str2, str3)
+str_index = str1[1]+str2[1]+str3[2]
+#print (str1[1],str2[1],str3[2]) # print index
+print (str_index)
+print (str2 [2:6]) # print range
